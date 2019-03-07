397ac25a296c8b1d76d7682abfcc6e0f14cfa852
vadymdk <vadim.dk81@gmail.com>
2019-03-07 21:54:55 +0200

    task3

---
+++ file.js
@@ -0,0 +1,47 @@
+/*Завдання 1*/
+function convert(amount) { 
+  let dollarRate = 8;
+   console.log( amount*dollarRate);
+ }
+
+convert(10) 
+
+/*Завдання 2*/
+/*function reverseString(str) {
+    return str.split("").reverse().join("");
+}
+reverseString("abc");*/
+
+
+/*function printStairs(n) { 
+    for(let i = 1; i <= n; i++) {
+        console.log( '#' .repeat(i))
+    }
+}
+
+printStairs(4)*/
+
+/*Завдання 3*/
+/*function range(start, end, step) {
+  let arr = []; //declare an empty array
+  var step = step || 1;//tests to see if step was supplied, otherwise it's 1
+  if(start < end) 
+  {
+    for(let i = start; i <= end; i += step)
+    {
+      arr.push(i);
+    }
+  }
+  
+  return arr;
+}
+function sum(array) {
+  let total = 0;
+  for(let i = 0; i < array.length; i++) 
+  { 
+    total += array[i];
+  }
+  return total;
+}
+
+console.log(sum(range(0,10)));*/
