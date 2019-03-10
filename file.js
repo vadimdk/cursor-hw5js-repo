faa65f15c753bbeed3b85d7ee95e4d520d877a53
vadymdk <vadim.dk81@gmail.com>
2019-03-10 20:26:44 +0200

    task7

--- file.js
+++ file.js
@@ -71,25 +71,47 @@ console.log(sum(range(0,10)));*/
 //min(3, 8, 4) 
 
 
+//function min(a, b, c) { 
+//    let result = Math.min(a,b,c);
+//         console.log(result)
+//     }
+//
+//min(10, 5, 11) 
+//min(3, 8, 4) 
+
+
+
     //Завдання 6 function which prints the pyramid picture of size n
-function printStairs(n) { 
-    for(let i = 1; i <= n; i++) {
-        if(i == 1) {
-        console.log( '.'.repeat(n-i),'#'.repeat(i));
-    }
-        else if(i == 2){
-            console.log( '.'.repeat(n-i),'#'.repeat(i+1));
-        }
-        else {
-            console.log( '.'.repeat(n-i),'#'.repeat(i+2));
-        }
-        }
+//function printStairs(n) { 
+//    for(let i = 1; i <= n; i++) {
+//        if(i == 1) {
+//        console.log( '.'.repeat(n-i),'#'.repeat(i));
+//    }
+//        else if(i == 2){
+//            console.log( '.'.repeat(n-i),'#'.repeat(i+1));
+//        }
+//        else {
+//            console.log( '.'.repeat(n-i),'#'.repeat(i+2));
+//        }
+//        }
+//}
+//
+//printStairs(3)
+
+
+      //Завдання 7 A function which transforms first and last letter to uppercase
+
+function firstAndLastToUpper(str) { 
+    let sln = str.length;
+    let lastLet = str.slice(-1);
+    let insideStr = str.slice(1,sln-1);
+    let lastLettoUpper = lastLet.toUpperCase();
+    let firstLet = str.slice(0,1);
+    let firstLettoUpper = firstLet.toUpperCase();
+    let resultStr = firstLettoUpper.concat(insideStr, lastLettoUpper);
+
+    console.log(resultStr);
+    
 }
 
-printStairs(3)
-
-
-
-
-
-
+firstAndLastToUpper('abcd')
