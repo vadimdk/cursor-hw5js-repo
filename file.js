08e21377e0ca9f8c388b7f4322c715f4901b8b80
vadymdk <vadim.dk81@gmail.com>
2019-03-10 17:28:00 +0200

    task6

--- file.js
+++ file.js
@@ -1,25 +1,29 @@
 /*Завдання 1*/
-function convert(amount) { 
-  let dollarRate = 8;
-   console.log( amount*dollarRate);
- }
+//function convert(amount) { 
+//  let rateDollar = 8;
+//   console.log( amount*dollarRate);
+// }
+//
+//convert(10) 
 
-convert(10) 
+
 
 /*Завдання 2*/
 /*function reverseString(str) {
     return str.split("").reverse().join("");
 }
-reverseString("abc");*/
+reverseString("abc")*/
+
+
 
 /*Завдання 3*/
-/*function printStairs(n) { 
-    for(let i = 1; i <= n; i++) {
-        console.log( '#' .repeat(i))
-    }
-}
-
-printStairs(4)*/
+//function printStairs(n) { 
+//    for(let i = 0; i <= n; i++) {
+//        console.log('#'.repeat(i))
+//    }
+//}
+//
+//printStairs(4)
 
 /*Завдання 4*/
 /*function range(start, end, step) {
@@ -45,3 +49,47 @@ function sum(array) {
 }
 
 console.log(sum(range(0,10)));*/
+
+
+
+    //  Завдання 5 a function which returns the smallest of three numbers
+
+//function min(a, b, c) { 
+//    if (a < b && a < c) {
+//     console.log(a)
+// }
+//    else if(b < a && b < c) {
+//        console.log(b)
+//    }
+//     else {
+//         console.log(c)
+//     }
+//
+//}
+//
+//min(10, 5, 11) 
+//min(3, 8, 4) 
+
+
+    //Завдання 6 function which prints the pyramid picture of size n
+function printStairs(n) { 
+    for(let i = 1; i <= n; i++) {
+        if(i == 1) {
+        console.log( '.'.repeat(n-i),'#'.repeat(i));
+    }
+        else if(i == 2){
+            console.log( '.'.repeat(n-i),'#'.repeat(i+1));
+        }
+        else {
+            console.log( '.'.repeat(n-i),'#'.repeat(i+2));
+        }
+        }
+}
+
+printStairs(3)
+
+
+
+
+
+
